edae864448f26e0c388f3b89270cc0b0616f7343
simhyeju <simhyeju89@gmail.com>
2025-03-09 19:07:47 +0900

    엔티티 개발 - 연관관계 있음

--- src/main/kotlin/com/hyeju/portfolio/domain/entitiy/Project.kt
+++ src/main/kotlin/com/hyeju/portfolio/domain/entitiy/Project.kt
@@ -3,11 +3,65 @@ package com.hyeju.portfolio.domain.entitiy
 import jakarta.persistence.*
 
 @Entity
-class Project : BaseEntity(){
+class Project( name:String,
+               description: String,
+               startYear: Int,
+               startMonth: Int,
+               endYear: Int?,
+               endMonth: Int?,
+               isActive: Boolean) : BaseEntity(){
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_id")
 
     var id: Long? =null
+
+    var name: String = name
+
+    var description: String = description
+
+    var startYear: Int = startYear
+
+    var startMonth: Int = startMonth
+
+    var endYear: Int? = endYear
+
+    var endMonth: Int? = endMonth
+
+    var isActive: Boolean = isActive
+
+    @OneToMany(targetEntity = ProjectDetail::class,
+        fetch = FetchType.LAZY,
+        cascade = [CascadeType.ALL])
+    //모든변화에다가 자식 엔티티에도 적용이 됨
+    @JoinColumn(name="project_id")
+    var details: MutableList<ProjectDetail> = mutableListOf()
+
+    @OneToMany(mappedBy = "project", fetch = FetchType.LAZY, cascade = [CascadeType.PERSIST])
+    var skills: MutableList<ProjectSkill> = mutableListOf()
+
+    fun getEndYearMonth(): String{
+        if (endYear == null || endMonth == null){
+            return "Present"
+        }
+
+        return "${endYear}.${endMonth}"
+    }
+
+    fun update(name:String, description: String, startYear: Int, startMonth: Int, endYear: Int?, endMonth: Int?, isActive: Boolean) {
+        this.name = name
+        this.description = description
+        this.startYear = startYear
+        this.startMonth = startMonth
+        this.endYear = endMonth
+        this.endMonth = endMonth
+        this.isActive = isActive
+    }
+
+    fun addDetails(details: MutableList<ProjectDetail>?){
+        if(details != null) {
+            this.details.addAll(details)
+        }
+    }
 }
